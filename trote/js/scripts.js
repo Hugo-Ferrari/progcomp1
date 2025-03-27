c597156873cfa0caf16d579667a2c4ffbdf10450
HUGO FERRARI PIRES <25981@unifacef.edu.br>
2025-03-27 20:37:37 -0300

    fazendo a calculadora

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,13 +2,14 @@ function calcular(){
     //vamos criar duas variaveis 
     //JS as variaveis nn possuem tipo
 
-    let mascote, homenagem
-    mascote = document.getElementById("mascote").value
-    alert(mascote)
+    let mascote, homenagem , leite, kitAvulso, suplementoAvulso
+    mascote = document.getElementById("mascote").value 
     homenagem = document.getElementById("homenagem").value
-    alert(homenagem)
+    leite = document.getElementById("leite").value
+    kitAvulso = document.getElementById("kitAvulso").value
+    suplementoAvulso = document.getElementById("suplementoAvulso").value
     // calcular a soma 
-
-    let soma =  Number(mascote) +  Number(homenagem)
-    alert(soma)
+    // recupera a quantidade de litros de leite 
+    let soma =  Number(mascote) +  Number(homenagem) + (2*Number(leite)) + (30*Number(kitAvulso)) + (15*Number(suplementoAvulso))
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
