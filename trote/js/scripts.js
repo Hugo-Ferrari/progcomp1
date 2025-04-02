5534f192d6a78ad69fc00e7a05a24b3444a0060b
HUGO FERRARI PIRES <25981@unifacef.edu.br>
2025-04-01 22:16:04 -0300

    fazendo a calcularora em js

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,14 +2,32 @@ function calcular(){
     //vamos criar duas variaveis 
     //JS as variaveis nn possuem tipo
 
-    let mascote, homenagem , leite, kitAvulso, suplementoAvulso
-    mascote = document.getElementById("mascote").value 
-    homenagem = document.getElementById("homenagem").value
-    leite = document.getElementById("leite").value
-    kitAvulso = document.getElementById("kitAvulso").value
-    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    let mascote, homenagem , leite, kit, suplemento, soma, equipe 
+    mascote = Number(document.getElementById("mascote").value )
+    // recupera a quantidade de litros de leite
+    homenagem = Number(document.getElementById("homenagem").value)
+    // recupera o valor da homenagem digitada pelo usuario 
+    leite = Number(document.getElementById("leite").value)
+    // recupera a quantidade de litros de leite
+    kit = Number(document.getElementById("kit").value)
+    // recupera a quantidade de kit
+    suplemento = Number(document.getElementById("suplemento").value)
     // calcular a soma 
-    // recupera a quantidade de litros de leite 
-    let soma =  Number(mascote) +  Number(homenagem) + (2*Number(leite)) + (30*Number(kitAvulso)) + (15*Number(suplementoAvulso))
+     
+    soma = mascote + homenagem + (2*(leite)) 
+     // vamos calcular a pontuação considerando metas de kitssuplemntos 
+     equipe =document.getElementById("equipe").value
+     if(equipe =="Laranja"){
+        if (kit>= 97){
+            soma=soma + 5000 + ((kit-97) * 30)
+        }
+        else if( kit >=49){
+            soma = soma + 2500 + ((kit-49) * 30)
+        }
+        else if (kit >= 19){
+            soma = soma + 1000 + ((kit-19) *30)
+        }
+     }
+
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
