c8ee54181d32ecac0326a713d0469ad608cd6553
HUGO FERRARI PIRES <25981@unifacef.edu.br>
2025-04-03 20:35:37 -0300

    colocando js nas tabelas dia 03.04.2025

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,7 +2,7 @@ function calcular(){
     //vamos criar duas variaveis 
     //JS as variaveis nn possuem tipo
 
-    let mascote, homenagem , leite, kit, suplemento, soma, equipe 
+    let mascote, homenagem , leite, kit, suplemento, sangue, soma, equipe 
     mascote = Number(document.getElementById("mascote").value )
     // recupera a quantidade de litros de leite
     homenagem = Number(document.getElementById("homenagem").value)
@@ -12,20 +12,26 @@ function calcular(){
     kit = Number(document.getElementById("kit").value)
     // recupera a quantidade de kit
     suplemento = Number(document.getElementById("suplemento").value)
+
+    sangue= Number(document.getElementById("sangue").value)
     // calcular a soma 
      
     soma = mascote + homenagem + (2*(leite)) 
      // vamos calcular a pontuação considerando metas de kitssuplemntos 
      equipe =document.getElementById("equipe").value
      if(equipe =="Laranja"){
-        if (kit>= 97){
-            soma=soma + 5000 + ((kit-97) * 30)
+
+        if (kit>= 97 && suplemento >= 49){
+            soma=soma + 5000 + ((kit-97) * 30) +((suplemento -49 ) *15)
         }
-        else if( kit >=49){
-            soma = soma + 2500 + ((kit-49) * 30)
+        else if( kit >=78 && suplemento >=35){
+            soma = soma + 4000 + ((kit-49) * 30) +((suplemento -39) *15)
         }
-        else if (kit >= 19){
-            soma = soma + 1000 + ((kit-19) *30)
+        else if (kit >= 49 && suplemento >= 25){
+            soma = soma + 2500 + ((kit-19) *30) + ((suplemento - 25)*15)
+        }
+        else if (kit >= 19 && suplemento >= 19){
+            soma = soma + 1000 + ((kit -19) *30) + ((suplemento - 10) *15)
         }
      }
 
