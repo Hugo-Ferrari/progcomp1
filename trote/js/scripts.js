d706f4882b07c46fda368a6d7c384ebb495a1f50
unknown <hugoferrari36@gmail.com>
2025-04-08 22:06:34 -0300

    sangue

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,7 +2,7 @@ function calcular(){
     //vamos criar duas variaveis 
     //JS as variaveis nn possuem tipo
 
-    let mascote, homenagem , leite, kit, suplemento, sangue,pacotedearroz5kg,pacotedearroz1kg,PacotedeFeijão02Kg,PacotedeFeijão01Kg,PacotedeMacarrãode400,Óleo900ml , soma, equipe 
+    let mascote, homenagem , leite, kit, suplemento, sangue,pacote5kg,pacotedearroz1kg,PacotedeFeijão02Kg,PacotedeFeijão01Kg,PacotedeMacarrãode400,Óleo900ml , soma, equipe 
     mascote = Number(document.getElementById("mascote").value )
     // recupera a quantidade de litros de leite
     homenagem = Number(document.getElementById("homenagem").value)
@@ -13,26 +13,25 @@ function calcular(){
     // recupera a quantidade de kit
     suplemento = Number(document.getElementById("suplemento").value)
 
-    sangue= Number(document.getElementById("sangue").value)
+    sangue = Number (document.getElementById ("sangue").value)
 
-    pacotedearroz5kg = Number(document.getElementById)(pacotedearroz5kg).value
+    pacote5kg = Number(document.getElementById("pacote5kg").value)
 
-    pacotedearroz1kg = Number(document.getElementById)(pacotedearroz1kg).value
+    pacotedearroz1kg = Number(document.getElementById("pacotedearroz1kg").value)
 
-    PacotedeFeijão02Kg = Number(document.getElementById)(PacotedeFeijão02Kg).value
+    PacotedeFeijão02Kg = Number(document.getElementById("PacotedeFeijão02Kg").value)
 
-    PacotedeFeijão01Kg = Number(document.getElementById)(PacotedeFeijão01Kg).value
+    PacotedeFeijão01Kg = Number(document.getElementById("PacotedeFeijão01Kg").value)
 
-    PacotedeMacarrãode400 = Number(document.getElementById)(PacotedeMacarrãode400).value
+    PacotedeMacarrãode400 = Number(document.getElementById("PacotedeMacarrãode400").value)
 
-    Óleo900ml = Number (document.getElementById)(Óleo900ml).value
+    Óleo900ml = Number(document.getElementById("Óleo900ml").value)
 
-    // calcular a soma 
-     
-    soma = mascote + homenagem + (2*(leite)) + (20*(sangue)) +(5* (pacotedearroz5kg)) + pacotedearroz1kg + (2*(PacotedeFeijão02Kg)) + PacotedeFeijão01Kg + (0.5*(PacotedeMacarrãode400)) + Óleo900ml
+    
      // vamos calcular a pontuação considerando metas de kitssuplemntos 
      equipe =document.getElementById("equipe").value
      if(equipe =="Laranja"){
+        // verifica kit e suplemento
 
         if (kit>= 97 && suplemento >= 49){
             soma=soma + 5000 + ((kit-97) * 30) +((suplemento -49 ) *15)
@@ -46,8 +45,14 @@ function calcular(){
         else if (kit >= 19 && suplemento >= 10){
             soma = soma + 1000 + ((kit -19) *30) + ((suplemento - 10) *15)
         }
+        if(sangue>= 49 ){
+            soma = soma+ 2500 + ((sangue -49) *20)
+            }
+            else {
+                soma = soma + (sangue * 20 )
+            }
      }
-     if(equipe =="Preta"){
+     else if(equipe =="Preta"){
 
         if (kit>= 103 && suplemento >= 52){
             soma=soma + 5000 + ((kit-103) * 30) +((suplemento -52) *15)
@@ -61,8 +66,14 @@ function calcular(){
         else if (kit >= 21 && suplemento >= 10){
             soma = soma + 1000 + ((kit -21) *30) + ((suplemento - 10) *15)
         }
+        if(sangue>= 52 ){
+            soma = soma+ 2500 + ((sangue -52) *20)
+            }
+            else {
+                soma = soma + (sangue * 20 )
+            }
      }
-     if(equipe =="Roxa"){
+     else if(equipe =="Roxa"){
 
         if (kit>= 102 && suplemento >= 51){
             soma=soma + 5000 + ((kit-102) * 30) +((suplemento -51) *15)
@@ -76,8 +87,14 @@ function calcular(){
         else if (kit >= 20 && suplemento >= 10){
             soma = soma + 1000 + ((kit -20) *30) + ((suplemento - 10) *15)
         }
+        if(sangue>= 51 ){
+            soma = soma+ 2500 + ((sangue -51) *20)
+            }
+            else {
+                soma = soma + (sangue * 20 )
+            }
      }
-     if(equipe =="Verde"){
+     else if(equipe =="Verde"){
 
         if (kit>= 88 && suplemento >= 44){
             soma=soma + 5000 + ((kit-88) * 30) +((suplemento -44) *15)
@@ -91,8 +108,14 @@ function calcular(){
         else if (kit >= 18 && suplemento >= 9){
             soma = soma + 1000 + ((kit -18) *30) + ((suplemento - 9) *15)
         }
+        if(sangue>= 44 ){
+            soma = soma+ 2500 + ((sangue -44) *20)
+            }
+            else {
+                soma = soma + (sangue * 20 )
+            }
      }
-     if(equipe =="Verde"){
+     else{
 
         if (kit>= 97 && suplemento >= 47){
             soma=soma + 5000 + ((kit-97) * 30) +((suplemento -47) *15)
@@ -106,7 +129,17 @@ function calcular(){
         else if (kit >= 19 && suplemento >= 9){
             soma = soma + 1000 + ((kit -19) *30) + ((suplemento - 9) *15)
         }
+        if(sangue>= 47 ){
+            soma = soma+ 2500 + ((sangue -47) *20)
+            }
+            else {
+                soma = soma + (sangue * 20 )
+            }
      }
+    }
+    
+     // calcular a soma 
+     
+    soma =soma+  mascote + homenagem + (2*leite) + (20*sangue) +(5* pacote5kg) + pacotedearroz1kg + (2*(PacotedeFeijão02Kg)) + PacotedeFeijão01Kg + (0.5*(PacotedeMacarrãode400)) + Óleo900ml
 
     document.getElementById("soma").innerHTML = `A soma é ${soma}`
-}
